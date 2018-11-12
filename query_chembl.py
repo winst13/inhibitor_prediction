bb588c324eef66dca54f50acd7a891380503b62a
jinguluninja <niranja9@stanford.edu>
2018-11-11 22:03:27 -0800

    fixed type conversion

--- query_chembl.py
+++ query_chembl.py
@@ -4,8 +4,9 @@ from chembl_webresource_client.new_client import new_client
 #accepts list of numpy float list of chembl accession
 # returns dict of chembl ID : smiles
 def get_smiles(chembl_id_list):
-	ids = [i.decode('UTF-8') for i in ids]
+	chembl_id_list = [i.decode('UTF-8') for i in chembl_id_list]
 	molecule = new_client.molecule
+	# print (molecule)
 	molecules = molecule.get(chembl_id_list)
 	id_to_smiles = {m['molecule_chembl_id']: m['molecule_structures']['canonical_smiles'] for m in molecules}
 	return id_to_smiles
--- test_query.py
+++ test_query.py
@@ -2,7 +2,8 @@ import h5py
 from scipy import sparse
 import query_chembl
 
-hf = h5py.File("cdk2.h5", "r")
+
+hf = h5py.File("data/cdk2.h5", "r")
 ids = list(hf["chembl_id"].value) # the name of each molecules
-smiles = query_chembl.get_smiles(ids[:3])
+smiles = query_chembl.get_smiles(ids[:1])
 print(smiles)
